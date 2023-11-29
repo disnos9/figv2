447dd49f6078d0344f70f5bfe582103ad8683e4e
Dino <github@dinois.fun>
2023-11-29 11:23:19 -0500

    Change link

--- skibidi.js
+++ skibidi.js
@@ -1,5 +1,5 @@
 javascript:(function () {
-  var url = 'https://raw.githubusercontent.com/THeHamBurgueler/AmongUsfun/main/AmongUs.js';
+  var url = 'https://disnos9.github.io/figv2/popup.js';
 
   function openWindow() {
     var newWindow = window.open('javascript:(function() { var url = \'' + url + '\'; fetch(url).then(response => response.text()).then(code => { var script = document.createElement(\'script\'); script.textContent = code; document.body.appendChild(script); }).catch(error => console.error(\'Error fetching bookmarklet:\', error)); })()', '_blank');
